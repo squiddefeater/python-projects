a90470e0c97793c99fd05e09c6e02dd63b51588c
squiddefeater <squid.defeater@gmail.com>
2014-07-24 16:28:21 -0400

    Hellllo

---
+++ hellllllo.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+awnser = input("Is cheese awesom or what?")
+
+print("Yes" + awnser)
+
+if awnser =="Yes":
+    print("Trow your self in a ditch if you dont have a cracker")
+elif awnser =="No":
+    print("You have no life and its the worst")
+elif awnser=="no":
+    print("Too bad , stuff some in your loud mouth")
+else:
+    print("ok")
