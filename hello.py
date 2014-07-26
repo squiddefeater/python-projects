66877fa82c228c105f26bff9a2a7da660a07efd0
squiddefeater <squid.defeater@gmail.com>
2014-07-26 13:09:38 -0400

    quiz

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ("hello world")
+yes_or_no= input ("do you like cheese?????")
+print (yes_or_no)
+if yes_or_no == "no":
+    print("you suck cracker")
+elif yes_or_no == "yes":
+    print ("you are awesome and boss")
+else:
+    print("well to bad")
---
+++ quiz.py
@@ -0,0 +1,12 @@
+awnser = input("do you like cheese and crakers?")
+print(awnser)
+if awnser == "yes":
+    print("you are awesome!")
+elif awnser == "no":
+    print("you suck crakers")
+elif awnser == "mabye":
+    print("make up your mind")
+elif awnser == "Yes":
+    print("you are awesome")
+else:
+    print("well to bad")
