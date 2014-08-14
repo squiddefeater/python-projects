f27de91af2477fa8aefe216f8d6d15ff28b339b2
squiddefeater <squid.defeater@gmail.com>
2014-08-14 16:31:30 -0400

    bridgekeeper2

---
+++ bridgekeeper2.py
@@ -0,0 +1,16 @@
+def go():
+    print("You may pass")
+def die():
+    print("You are thrown into the pit eternal peril")
+
+
+
+       name = input("what is your name?")
+   Quest = input ("what is your quest")
+   color = input ("what is your favoite color")
+if color == "blue":
+    go()
+else:
+    die()
+        
+         
